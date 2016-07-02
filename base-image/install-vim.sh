fe3e1dfa23b9c78d549e9ed403a9081c3c210258
Gregory Daynes <gregdaynes@gmail.com>
2016-07-02 11:57:17 -0700

    completed vim install script, added vimrc + vundle

--- base-image/install-vim.sh
+++ base-image/install-vim.sh
@@ -2,3 +2,6 @@ apk update && apk add \
     vim \
     && rm -rf /var/cache/apk/*
 
+curl -o ~/.vimrc https://raw.githubusercontent.com/gregdaynes/microservices.wtf/master/vim-install/vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
